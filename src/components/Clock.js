7cad3c4fe0a3042527f4794cc46d407550d19a62
Adam Basiak <panadambasiak@gmail.com>
2022-04-30 21:17:42 +0200

    add prop-types

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
-const Clock = ({className="", minutes=.5, seconds=0 }) => {
+const Clock = ({className, minutes, seconds }) => {
     return (
         <h2 className={`Clock ${className}`}>
             Pozostało {minutes <=0 ? '00' : minutes }:{seconds <= 0 ? '00' : seconds}
         </h2>
     )
+}
+
+Clock.defaultProps = {
+    className: ''
+}
+
+const numberOrstringType = propTypes.oneOfType([propTypes.string, propTypes.number])
+Clock.propTypes = {
+    className: propTypes.string.isRequired,
+    minutes: propTypes.number.isRequired,
+    seconds: numberOrstringType.isRequired
 }
 
 export default Clock;
